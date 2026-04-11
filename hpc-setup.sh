5eef036870b62709cb2ec3437c9f76671c787350
davidm027 <davidmalone@cpp.edu>
2026-04-11 11:12:57 -0700

    Separate building from conda env, pt 2

--- hpc-setup.sh
+++ hpc-setup.sh
@@ -5,7 +5,5 @@ conda create -n my-env -c conda-forge gxx_linux-64=13 gcc_linux-64=13 cmake make
 conda activate my-env
 
 # Build
-mkdir -p build/
-
-cmake -S . -B build/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$CONDA_PREFIX"
-cmake --build build/ --parallel
+chmod +x build.sh
+./build.sh
